1a4289994e02411cd124a01f39ef30e7bfc0c1e2
hanish <151109119+hanishP3@users.noreply.github.com>
2024-04-29 11:06:34 +0000

    Numbers and Maths

---
+++ jscode05/numbers.js
@@ -0,0 +1,14 @@
+let number = 58965.6898
+console.log(number.toPrecision(5));
+
+let number2 = 1000000
+console.log(number2.toLocaleString('en-IN'));
+
+//maths
+console.log(Math);
+console.log(Math.round(7889.9090)) //WHEN WE DONT WANT THE DECIMAL NUMBER. 
+console.log(Math.abs(-4));
+console.log(Math.abs(4));
+console.log(Math.ceil(13.2)) //IF THE NUMBER IS HAVING A DECIMAL POINT VALUE, IT GIVES THE HIGHER NUMBER.
+let randomNum=(Math.random()*6+1);
+console.log(Math.round(randomNum))
