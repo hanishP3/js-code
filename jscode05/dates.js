2a79e1a488078cf71f7bf8c0cc249c6f9e73d625
hanish <151109119+hanishP3@users.noreply.github.com>
2024-05-01 07:42:33 +0000

    Date and Time

---
+++ jscode05/dates.js
@@ -0,0 +1,9 @@
+//DATES
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+//String interpolation
+console.log(`the follwoing day in this system is ${myDate.getDay()}`)
