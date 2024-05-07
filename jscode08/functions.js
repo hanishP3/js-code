a8a91f94e24d87c4bd8a6b262f70b218e176fc8a
hanish <151109119+hanishP3@users.noreply.github.com>
2024-05-07 08:54:48 +0000

    functions

---
+++ jscode08/functions.js
@@ -0,0 +1,17 @@
+//FUNTCION DEFINATION
+function myFun(){
+    console.log("leo")
+    console.log("leo")
+    console.log("leo")
+}
+//CALLING A FUNCTION
+myFun();
+//ADD TWO NUMBER 
+
+function addNum(number1,number2){           //PARAMETERS
+    let number3 = (number1+number2)
+    return number3                          //AFTER RETURN STATEMENT NOTHING executes
+    console.log(number3)
+
+}
+addNum(99,89)                               //ARGUMENTS 
