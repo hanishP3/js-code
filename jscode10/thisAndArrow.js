29e3988f6ac4e4bf6df7775c6ed57ac939e014be
hanish <151109119+hanishP3@users.noreply.github.com>
2024-05-14 11:20:41 +0000

    This and Arrow

--- jscode10/thisAndArrow.js
+++ jscode10/thisAndArrow.js
@@ -1,1 +1,25 @@
-console.log("hello wotld")
+// //THIS KEYWORD
+// // THIS KEYWORD REFERS TO THE CURRENT VALUE IN THE SCOPE
+// const myData = {
+//     name:"hanish",
+//     age:21,
+//     favcolor:"black",
+
+//    myfunction() {
+//     console.log(`this is a this keyword used output the name is ${this.name} and the age is ${this.age}`);
+//    }
+
+// }
+// myData.myfunction();
+// name:"leo"
+// myfunction();
+
+//ARROW FUNCTION
+// const myArrow = (num1,num2) => {            //we can use => arrow instead of function keyword
+//     return num1+num2
+// } 
+// console.log(myArrow(1,4))
+
+//BY USING ARROW WE CAN DO IMPLICIT 
+const myArrow02 = (num1,num2) => ( num1*num2 )       //NEED NOT TO WRITE THE CURLY BRACES AND RETURN ST BEACUSE WE HAVE ONLY ONE ST
+console.log(myArrow02(23,89));
