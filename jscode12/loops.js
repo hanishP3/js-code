a763bae00241ec4f001839a7dd8bc3c58bcb51b4
hanish <151109119+hanishP3@users.noreply.github.com>
2024-05-16 05:35:12 +0000

    loop

---
+++ jscode12/loops.js
@@ -0,0 +1,12 @@
+//FOR LOOP
+
+for(let i = 0;i<=5;i++){
+    console.log(` the  value of i is  ${i}`)
+}
+
+//WHILE LOOPS
+let i =1
+while(i<=100){
+    console.log(`the value of i is ${i}`)
+    i++
+}
