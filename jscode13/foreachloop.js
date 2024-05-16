0cc751be6c478735e21574a8ad83d3ae65fe03cb
hanish <151109119+hanishP3@users.noreply.github.com>
2024-05-16 11:03:13 +0000

    Foreach

---
+++ jscode13/foreachloop.js
@@ -0,0 +1,14 @@
+//for each loop
+const myArr = ["apple","banana","cat","dog"]
+
+myArr.forEach( function (val) {
+    console.log(val)
+});
+
+//OR
+
+myArr.forEach( vall => {
+    console.log(`2*2 and ${vall}`)
+})
+
+console.log(2*2)
